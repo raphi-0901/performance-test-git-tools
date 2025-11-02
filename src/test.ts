1f02522093863132cd8799b8f73d749fb5652cc9
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-11-02 16:38:22 +0100

    feat(test): add dry-run mode and new patches
    - introduce a `--dry-run` flag to the main test script to skip auto-commits
    - in dry-run mode, create historical commits with a "dry-run" message instead
    - extend the test script to run two new patches for menu component feature and refactor

--- src/test.ts
+++ src/test.ts
@@ -2,12 +2,18 @@ import {createHistoricalCommit} from "./utils/create-historical-commit.js";
 import {initializeGitRepoWithSkeleton} from "./utils/initialize-git-repo-with-skeleton.js";
 import {createAutoCommitUsingPatch} from "./utils/create-auto-commit-using-patch.js";
 
+const args = process.argv.slice(2);
+const hasFlag = (flagName: string) => args.includes(`--${flagName}`);
+const dryRun = hasFlag('dry-run');
+
 await initializeGitRepoWithSkeleton()
 await createHistoricalCommit('Test Commit');
 await createHistoricalCommit('Second Test Commit');
 
-await createAutoCommitUsingPatch('1-install-tailwind')
-await createAutoCommitUsingPatch('2-install-color-mode')
-await createAutoCommitUsingPatch('3-create-menu-component')
+await createAutoCommitUsingPatch('1-install-tailwind', dryRun)
+await createAutoCommitUsingPatch('2-install-color-mode', dryRun)
+await createAutoCommitUsingPatch('3-create-menu-component', dryRun)
+await createAutoCommitUsingPatch('4-feature-menu-component', dryRun)
+await createAutoCommitUsingPatch('5-refactor-menu-component', dryRun)
 
 
--- src/utils/create-auto-commit-using-patch.ts
+++ src/utils/create-auto-commit-using-patch.ts
@@ -2,9 +2,15 @@ import dayjs, { type Dayjs } from 'dayjs';
 import { git } from './constants.js';
 import {applyPatch} from "./apply-patch.js";
 import {createAutoCommit} from "./create-auto-commit.js";
+import {createHistoricalCommit} from "./create-historical-commit.js";
 
-export async function createAutoCommitUsingPatch(patchName: string, day: Dayjs = dayjs()) {
+export async function createAutoCommitUsingPatch(patchName: string, dryRun = false, day: Dayjs = dayjs()) {
     await applyPatch(patchName);
     await git.add('.')
-    await createAutoCommit(day)
+
+    if(!dryRun) {
+        await createAutoCommit(day)
+    } else {
+        await createHistoricalCommit(`dry-run commit of patch: ${patchName}`, day)
+    }
 }
